5cbbc256ba2330fea2e55bc178e0ab8d85855b6d
TanishqJangir <tanishqjangir001@gmail.com>
2026-01-17 23:57:09 +0530

    ts

--- Part2/src/LearnedOneByOne.ts
+++ Part2/src/LearnedOneByOne.ts
@@ -346,4 +346,30 @@
 
 
 
+// interface User {
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+// }
 
+// let user: User[] = [
+//     {
+//         firstName: "skdfjks",
+//         lastName: "skjfkld",
+//         age: 44
+//     },
+//     {
+//         firstName: "skdfjks",
+//         lastName: "skjfkld",
+//         age: 10
+//     }
+
+// ]
+
+// function isEligible(users: User[]) {
+//     return users.filter((element) => element.age >= 18)
+// }
+
+// const filteredUser = isEligible(user);
+
+// console.log(filteredUser)
